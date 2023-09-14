dc038ecfb21b4b2151fac3f9611f6447d7d66b43
João <joao_bernardi@outlook.com>
2023-09-14 17:40:55 -0300

    `Spring Boot` - Init

---
+++ pdf/src/main/java/br/com/im9/pdf/Im9PdfProcessorApplication.java
@@ -0,0 +1,13 @@
+package br.com.im9.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Im9PdfProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Im9PdfProcessorApplication.class, args);
+	}
+
+}
