9cab19555941ab3f5dd0233b2b68155c5a1bd2bf
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-04-02 09:08:08 -0300

    `Main` - Add elastic middleware

--- pdf/src/main/java/br/com/im9/pdf/Im9PdfProcessorApplication.java
+++ pdf/src/main/java/br/com/im9/pdf/Im9PdfProcessorApplication.java
@@ -3,10 +3,13 @@ package br.com.im9.pdf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 @SpringBootApplication
 public class Im9PdfProcessorApplication {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(Im9PdfProcessorApplication.class, args);
 	}
 
